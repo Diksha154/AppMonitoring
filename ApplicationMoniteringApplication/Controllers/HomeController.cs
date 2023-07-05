cd3d4beda8528978934da71ff080f77045393218
Diksha <dikshagoyal15498@gmail.com>
2023-07-05 16:11:04 +0530

    Code refectoring

--- ApplicationMoniteringApplication/Controllers/HomeController.cs
+++ ApplicationMoniteringApplication/Controllers/HomeController.cs
@@ -27,20 +27,12 @@ namespace ApplicationMoniteringApplication.Controllers
             string duration_val = duration;
             string cat_val = category;
 
-            //string cat_val = Request.Form["Category"];  //or
-            //cat_val = Request["Category"];
-            //string duration_val = Request.Form["Duration"];  //or
-            //duration_val = Request["Duration"];  
-
             if (!string.IsNullOrEmpty(cat_val) && !string.IsNullOrEmpty(duration_val))
             {
                 if (cat_val != "0" && duration_val != "0")
                 {
                     HomeBusiness homeBusiness = new HomeBusiness();
 
-                    //Daily
-                    //if (duration_val == "1")
-                    //{
                     if (cat_val == "1")//Longest running job 
                     {
                         var filterData = homeBusiness.GetLongestRuningJobs(duration_val);
@@ -61,57 +53,10 @@ namespace ApplicationMoniteringApplication.Controllers
                         var filterData = homeBusiness.GetHightCriticalIncidents(duration_val);
                         return Json(filterData, JsonRequestBehavior.AllowGet);
                     }
-                    //}
-
                     else
                     {
                         return new JsonResult();
                     }
-                    //Monthly
-                    //else if (duration_val == "2")
-                    //{
-                    //    if (cat_val == "1") //Longest running job
-                    //    {
-                    //        filterdData = homeBusiness.GetLongestRuningJobs(duration_val);
-                    //        return View("Index", filterdData);
-                    //    }
-                    //    else if (cat_val == "2")//Maximum number of failures 
-                    //    {
-                    //        filterdData = homeBusiness.GetMaxFailureApp(duration_val);
-                    //        return View("Index", filterdData);
-                    //    }
-                    //    else if (cat_val == "3") //Maximum number of recurring job failures 
-                    //    {
-
-                    //    }
-                    //    else if (cat_val == "4") //P1-P2 incidents 
-                    //    {
-
-                    //    }
-                    //}
-
-                    ////Yearly
-                    //else if (duration_val == "3")
-                    //{
-                    //    if (cat_val == "1")//Longest running job 
-                    //    {
-                    //        filterdData = homeBusiness.GetLongestRuningJobs(duration_val);
-                    //        return View("Index", filterdData);
-                    //    }
-                    //    else if (cat_val == "2") //Maximum number of failures                         {
-                    //    {
-                    //        filterdData = homeBusiness.GetMaxFailureApp(duration_val);
-                    //        return View("Index", filterdData);
-                    //    }
-                    //    else if (cat_val == "3")//Maximum number of recurring job failures
-                    //    {
-
-                    //    }
-                    //    else if (cat_val == "4") //P1-P2 incidents 
-                    //    {
-
-                    //    }
-                    //}
                 }
                 else
                 {
@@ -122,37 +67,7 @@ namespace ApplicationMoniteringApplication.Controllers
             {
                 return new JsonResult();
             }
-
-
-            return new JsonResult();
-
-            //IndexFilteredModel indexFilteredModel1 = new IndexFilteredModel()
-            //{
-            //    ApplicationName = "abc1",
-            //    JobName = "job1",
-            //    Runtime = "234",
-            //    ScheduleTypeId = "1"
-            //};
-            //IndexFilteredModel indexFilteredModel2 = new IndexFilteredModel()
-            //{
-            //    ApplicationName = "def1",
-            //    JobName = "job2",
-            //    Runtime = "234",
-            //    ScheduleTypeId = "1"
-            //};
-            //IndexFilteredModel indexFilteredModel3 = new IndexFilteredModel()
-            //{
-            //    ApplicationName = "ghi1",
-            //    JobName = "job3",
-            //    Runtime = "234",
-            //    ScheduleTypeId = "1"
-            //};
-
-            //filterdData.Add(indexFilteredModel1);
-            //filterdData.Add(indexFilteredModel2);
-            //filterdData.Add(indexFilteredModel3);
-
-            //return View("Index", filterdData);
+            return new JsonResult();            
         }
 
         public JsonResult GetIncidentData(string id)
@@ -165,7 +80,6 @@ namespace ApplicationMoniteringApplication.Controllers
 
             var filterData = homeBusiness.GetIncidentData(id);
             return Json(filterData, JsonRequestBehavior.AllowGet);
-            //return new JsonResult();
         }
 
         public ActionResult GetJobData(string id, string duration)
